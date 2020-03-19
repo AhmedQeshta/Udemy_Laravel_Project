0a66b66b5e27105d6122f86ac795403759e60370
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2020-03-18 21:31:03 -0800

    start a dynamic and  sec 6.3 (24)

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -1,67 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact show</title>
-       <!-- Styles -->
-       <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+@extends('layouts.main')
 
-        .full-height {
-            height: 100vh;
-        }
+@section('content')
+<main class="py-5">
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header card-title">
+              <strong>Contact Details</strong>
+            </div>           
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group row">
+                    <label for="first_name" class="col-md-3 col-form-label">First Name</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->first_name }}</p>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="last_name" class="col-md-3 col-form-label">Last Name</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->last_name }}</p>
+                    </div>
+                  </div>
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
+                  <div class="form-group row">
+                    <label for="email" class="col-md-3 col-form-label">Email</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->email }}</p>
+                    </div>
+                  </div>
 
-        .position-ref {
-            position: relative;
-        }
+                  <div class="form-group row">
+                    <label for="phone" class="col-md-3 col-form-label">Phone</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->phone }}</p>
+                    </div>
+                  </div>
 
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-</head>
-<body>
-    <h1>Contact details</h1>
-    <div>{{ $contact }}</div>
-    <a href="{{ route('contacts.index') }}">Back to all contacts</a>
-</body>
-</html>
+                  <div class="form-group row">
+                    <label for="name" class="col-md-3 col-form-label">Address</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->address }}</p>
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="company_id" class="col-md-3 col-form-label">Company</label>
+                    <div class="col-md-9">
+                      <p class="form-control-plaintext text-muted">{{ $contact->company->email }}</p>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="form-group row mb-0">
+                    <div class="col-md-9 offset-md-3">
+                        <a href="#" class="btn btn-info">Edit</a>
+                        <a href="#" class="btn btn-outline-danger">Delete</a>
+                        <a href="{{ route('contacts.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
+@endsection
